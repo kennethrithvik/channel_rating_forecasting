ece41a3a18971933a655a4142647424c2d5f812e
kennethrithvik <kennethrithvik@yahoo.in>
2018-09-17 13:07:56 +0800

    plot changes

--- v2/rita_assg.R
+++ v2/rita_assg.R
@@ -1,4 +1,3 @@
-library(xlsx)
 library(tidyr)
 library(Metrics)
 library(smooth)
@@ -6,11 +5,11 @@ library(TTR)
 library(forecast)
 library(tseries)
 
-setwd('F:\\ISS KE\\Semester 2\\developing intelligent systems for performing business analytics\\Lecture Notes\\Rita - Day 5 Material')
-getwd()
-data <- read.xlsx("ActualRatings_weeklyGRP.xls",sheetIndex = 2)
-data <- separate(data = data , col = GRPRatingsDate, into = c("Date", "wk_num"), sep = 12)
+data <- read_csv("rating.csv", col_names = FALSE,col_types = cols(X3 = col_skip(), X4 = col_skip()))
+data <- separate(data = data , col = X1, into = c("Date", "wk_num"), sep = 12)
 data <- data[,-2]
+data["GRP"]<-data$X2
+
 t_data <- ts (data$GRP, start = c(2007,25), frequency = 52) # frequency 4 => Quarterly Data
 plot(t_data)
 # splitting data into train and test as per the CA requirements
@@ -34,30 +33,38 @@ test_dat <- data_1[73:length(data_1$GRP),]
 lm_model <- lm(GRP ~ num, train_dat)
 summary(lm_model)
 test_pred <- predict(lm_model, test_dat)
+train_pred <- predict(lm_model, train_dat)
 lm_test_ts <- ts(test_pred,start=c(2008,45),frequency = 52)
+lm_train_ts <- ts(train_pred,start=c(2007,25),frequency = 52)
 # Metrics and plot of the time series regression output.
 rmse(test_dat$GRP,test_pred)
 MAPE(test_dat$GRP,test_pred)
 mse(test_dat$GRP,test_pred)
 mad(test_dat$GRP,test_pred)
-ts.plot(t_data,lm_test_ts,col=c("blue","red"), type="o", main = "Time Series Regression", sub = "Original vs Predicted", ylab = "Ratings")
-legend("topright", legend = c("Original","Predicted"), col = c("blue","red"), lty = 1)
+ts.plot(t_data,lm_test_ts,lm_train_ts,col=c("blue","red","green"), type="o", main = "Time Series Regression", sub = "Original vs Predicted", ylab = "Ratings")
+legend("topright", legend = c("Original","Predicted Train","Predicted Test"), col = c("blue","green","red"), lty = 1)
 
 # simple exponential smoothing
 es_s_model <- HoltWinters(train_ts,beta = FALSE, gamma = FALSE)
 es_s_model
-es_s_pred <- predict(es_s_model, 20, prediction.interval = FALSE)
-ts.plot(t_data,es_s_pred,col=c("red","blue"))
-rmse(test_dat$GRP,es_s_pred)
-MAPE(test_dat$GRP,es_s_pred)
+es_s_pred <- forecast(es_s_model, h=20)
+ts.plot(t_data,es_s_pred$mean,es_s_pred$fitted,col=c("red","blue","green"),type="o", main = "SIngle Exponential Smoothing", sub = "Original vs Predicted", ylab = "Ratings")
+legend("topright", legend = c("Original","Predicted Train","Predicted Test"), col = c("red","green","blue"), lty = 1)
+rmse(test_dat$GRP,es_s_pred$mean)
+MAPE(test_dat$GRP,es_s_pred$mean)
+mse(test_dat$GRP,es_s_pred$mean)
+mad(test_dat$GRP,es_s_pred$mean)
 
 # double exponential smoothing
 es_d_model <- HoltWinters(train_ts, gamma = FALSE)
 es_d_model
-es_d_pred <- predict(es_d_model, 20, prediction.interval = FALSE)
-ts.plot(t_data,es_d_pred,col=c("red","blue"))
-rmse(test_dat$GRP,es_d_pred)
-MAPE(test_dat$GRP,es_d_pred)
+es_d_pred <- forecast(es_d_model, h=20)
+ts.plot(t_data,es_d_pred$mean,es_d_pred$fitted,col=c("red","blue","green"), type="o", main = "Holt's Method", sub = "Original vs Predicted", ylab = "Ratings")
+#legend("topright", legend = c("Original","Predicted Train","Predicted Test"), col = c("blue","green","red"), lty = 1)
+rmse(test_dat$GRP,es_d_pred$mean)
+MAPE(test_dat$GRP,es_d_pred$mean)
+mse(test_dat$GRP,es_d_pred$mean)
+mad(test_dat$GRP,es_d_pred$mean)
 
 # triple exponential smoothing - no seasonality - not required
 
@@ -76,9 +83,11 @@ auto.arima(train_ts, seasonal = FALSE)
 fit1 = arima(train_ts, order = c(1,1,1))
 fcast <- forecast(fit1, h = 20)
 plot.ts(t_data,col=c("red","blue"))
-ts.plot(t_data,fcast$mean,col=c("red","blue"))
+ts.plot(t_data,fcast$mean,fcast$fitted,col=c("red","blue","green"), type="o", main = "ARIMA", sub = "Original vs Predicted", ylab = "Ratings")
+legend("topright", legend = c("Original","Predicted Train","Predicted Test"), col = c("blue","green","red"), lty = 1)
 rmse(test_dat$GRP, fcast$mean)
 MAPE(test_dat$GRP, fcast$mean)
-
+mse(test_dat$GRP,fcast$mean)
+mad(test_dat$GRP,fcast$mean)
 
 
